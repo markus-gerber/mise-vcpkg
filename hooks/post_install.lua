09200cb2f0ab32a13af95fe1db0d0565b40c4f51
Markus Gerber <markus.gerber@fau.de>
2026-05-25 22:31:59 +0200

    ignore output during installation

--- hooks/post_install.lua
+++ hooks/post_install.lua
@@ -6,6 +6,11 @@ function PLUGIN:PostInstall(ctx)
 
     print("Cloning vcpkg repository...")
     local clone_cmd = "git clone https://github.com/microsoft/vcpkg.git " .. path
+    if os_type == "windows" then
+        clone_cmd = clone_cmd .. " >NUL 2>&1"
+    else
+        clone_cmd = clone_cmd .. " >/dev/null 2>&1"
+    end
     local res = os.execute(clone_cmd)
     if res ~= 0 then
         error("Failed to clone vcpkg repository")
@@ -13,6 +18,11 @@ function PLUGIN:PostInstall(ctx)
 
     print("Checking out version " .. version .. "...")
     local checkout_cmd = "cd " .. path .. " && git checkout " .. version
+    if os_type == "windows" then
+        checkout_cmd = checkout_cmd .. " >NUL 2>&1"
+    else
+        checkout_cmd = checkout_cmd .. " >/dev/null 2>&1"
+    end
     res = os.execute(checkout_cmd)
     if res ~= 0 then
         error("Failed to checkout version " .. version)
@@ -21,9 +31,9 @@ function PLUGIN:PostInstall(ctx)
     print("Bootstrapping vcpkg...")
     local bootstrap_cmd = ""
     if os_type == "windows" then
-        bootstrap_cmd = "cd " .. path .. " && .\\bootstrap-vcpkg.bat -disableMetrics"
+        bootstrap_cmd = "cd " .. path .. " && .\\bootstrap-vcpkg.bat -disableMetrics >NUL 2>&1"
     else
-        bootstrap_cmd = "cd " .. path .. " && ./bootstrap-vcpkg.sh -disableMetrics"
+        bootstrap_cmd = "cd " .. path .. " && ./bootstrap-vcpkg.sh -disableMetrics >/dev/null 2>&1"
     end
 
     res = os.execute(bootstrap_cmd)
